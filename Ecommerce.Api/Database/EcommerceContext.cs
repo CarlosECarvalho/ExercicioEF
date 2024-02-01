2eea53aa309adedf3a76fa6bc04bc94ac5c2e197
Carlos  Eduardo Ferreira Carvalho <carlos_ferreira@id.uff.br>
2024-02-01 00:18:22 -0300

    Configurações de acesso ao BD e configuração do EF. Mapeamento inicial de classes

--- Ecommerce.Api/Database/EcommerceContext.cs
+++ Ecommerce.Api/Database/EcommerceContext.cs
@@ -1,6 +1,26 @@
-﻿namespace Ecommerce.Api.Database
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ecommerce.Api.Database
 {
-    public class EcommerceContext
+    public class EcommerceContext : DbContext
     {
+        public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
+        public DbSet<Departamento> Departamentos { get; set;}
+
+        #region Conexao sem distinção de ambiente
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=PC-CADU\\SQLEXPRESS;Initial Catalog=EcommerEF ;Integrated Security=True");
+        //    base.OnConfiguring(optionsBuilder);
+
+
+        //}
+        #endregion
+
     }
 }
--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,4 +1,7 @@
+global using Ecommerce.models;
+using Ecommerce.Api.Database;
 using Ecommerce.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Api
 {
@@ -7,6 +10,7 @@ namespace Ecommerce.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration; //essa configuração servirá para o controle de banco de dados
 
             #region ConfigureService()
             // Add services to the container.
@@ -17,6 +21,8 @@ namespace Ecommerce.Api
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<IUsuarioRepository,UsuarioRepository>(); //adiciono a forma de gerenciamento do ciclo de vida da instanciação no conteiner de injeção
             //permite que eu altere a instanciação apenas trocando a classe concreta que será instanciada, podendo fazer a troca de banco de dados, ou para um moq de testes
+            builder.Services.AddDbContext<EcommerceContext>( options => options.UseSqlServer(configuration.GetConnectionString("Ecommerce")));//implemento o context do meu db ao projeto, com a expressão lambda indico a variável configuration com a propriedade GetConnection indico o nome da propriedade do banco no Json
+
             #endregion
 
             var app = builder.Build();
